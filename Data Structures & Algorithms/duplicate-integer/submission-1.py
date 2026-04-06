4ae35df3de2731a5993c45faf21560d8408bcaad
tbh20 <apollosunmk@gmail.com>
2026-04-07 01:42:48 +0800

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasDuplicate(self, mnus: List[int]) -> bool:
+        #use one loop to iterate throguh the list and check each num
+        #set data type remove duplicates
+        #create a set copy
+        #after removing duplicates, only unique values will remain 
+
+        if len(mnus) != len(set(mnus.copy())):
+            return True
+        else:
+            return False 
+
